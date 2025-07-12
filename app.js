2605b553d0622adf598b095975671ff888d71d98
HakimaNoori <hakima.noori.1379@gmail.com>
2025-07-12 21:46:46 +0430

    Add app.js and package.json

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const menuRoutes = require("./routes/menuRoutes");
+const restaurantRoutes = require("./routes/restaurantRoutes");
+const customerRoutes = require("./routes/customerRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use("/menu-items", menuRoutes);
+app.use("/customers", customerRoutes);
+app.use("/orders", orderRoutes);
+app.use("/restaurants", restaurantRoutes);
+
+app.listen(port, () => {
+  console.log(`Server is running on port localhost:${port}`);
+});
+
+
