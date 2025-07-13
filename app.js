7fa3555dad7c0ca2da15b785362f0112198df158
HakimaNoori <hakima.noori.1379@gmail.com>
2025-07-13 14:24:35 +0430

    add app.js file

--- app.js
+++ app.js
@@ -1,28 +1,3 @@
-// const express = require("express");
-// const menuRoutes = require("./routes/menuRoutes");
-// const restaurantRoutes = require("./routes/restaurantRoutes");
-// const customerRoutes = require("./routes/customerRoutes");
-// const orderRoutes = require("./routes/orderRoutes");
-// require("dotenv").config();
-// const app = express();
-// const port = process.env.PORT || 3000;
-
-// app.use(express.json());
-// app.use("/", menuRoutes);
-// app.use("/customers", customerRoutes);
-// app.use("/orders", orderRoutes);
-// app.use("/restaurants", restaurantRoutes);
-
-// app.get("/", (req, res) => {
-//   res.send("🎉 Foodie Hub API is running!");
-// });
-
-// app.listen(port, () => {
-//   console.log(`Server is running on port localhost:${port}`);
-// });
-
-
-
 const express = require("express");
 const menuRoutes = require("./routes/menuRoutes");
 const restaurantRoutes = require("./routes/restaurantRoutes");
