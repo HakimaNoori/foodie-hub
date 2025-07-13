21e014c5eaedbfaee06e90e931eb06a04637a928
HakimaNoori <hakima.noori.1379@gmail.com>
2025-07-13 13:51:52 +0430

    change app.js file

--- app.js
+++ app.js
@@ -1,14 +1,47 @@
+// const express = require("express");
+// const menuRoutes = require("./routes/menuRoutes");
+// const restaurantRoutes = require("./routes/restaurantRoutes");
+// const customerRoutes = require("./routes/customerRoutes");
+// const orderRoutes = require("./routes/orderRoutes");
+// require("dotenv").config();
+// const app = express();
+// const port = process.env.PORT || 3000;
+
+// app.use(express.json());
+// app.use("/", menuRoutes);
+// app.use("/customers", customerRoutes);
+// app.use("/orders", orderRoutes);
+// app.use("/restaurants", restaurantRoutes);
+
+// app.get("/", (req, res) => {
+//   res.send("🎉 Foodie Hub API is running!");
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port localhost:${port}`);
+// });
+
+
+
 const express = require("express");
 const menuRoutes = require("./routes/menuRoutes");
 const restaurantRoutes = require("./routes/restaurantRoutes");
 const customerRoutes = require("./routes/customerRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 require("dotenv").config();
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use("/menu-items", menuRoutes);
+
+// ✅ ابتدا این روت تستی رو بذار
+app.get("/", (req, res) => {
+  res.send("🎉 Foodie Hub API is running!");
+});
+
+// ✅ بقیه‌ی روت‌ها با مسیرهای مشخص
+app.use("/menus", menuRoutes);
 app.use("/customers", customerRoutes);
 app.use("/orders", orderRoutes);
 app.use("/restaurants", restaurantRoutes);
